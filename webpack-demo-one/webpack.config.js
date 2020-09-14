fdf2cde67300bfb5b20401ffca4e698ed70ae327
dengs.official <dengs.official@gmail.com>
2020-09-14 22:13:37 +0800

    feat: 引入html-webpack-plugin自动将打包内容插入模版

--- webpack-demo-one/webpack.config.js
+++ webpack-demo-one/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const r = (p) => path.resolve(__dirname, p);
 
@@ -8,5 +9,17 @@ module.exports = {
   output: { // 出口
     filename: 'bunlde.js', //文件名
     path: r('dist') // 路径，必须为绝对路径
+  },
+  plugins: [ // 插件
+    new HtmlWebpackPlugin({ // html插件, 将js/css引入html模版
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: { // 开发服务器配置
+    contentBase: './dist',
+    port: 3000,
+    host: '0.0.0.0',
+    compress: true
   }
 }
