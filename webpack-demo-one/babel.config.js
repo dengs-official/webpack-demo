40bd241586da32ce99ab1b6a614c7abc2892a2fc
dengs.official <dengs.official@gmail.com>
2020-09-18 23:47:45 +0800

    feat: 更换@babel/runtime 为 @babel/runtime-corejs3

--- webpack-demo-one/babel.config.js
+++ webpack-demo-one/babel.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   presets: [
-    '@babel/preset-env'
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: false,
+      }
+    ]
   ],
   plugins: [
-    '@babel/plugin-transform-runtime', // 打包时动态引入，避免重复定义
-    '@babel/plugin-proposal-class-properties'
-  ]
+    [
+      '@babel/plugin-transform-runtime', // 打包时动态引入，避免重复定义
+      {
+        corejs: 3
+      }
+    ]
+  ],
+  exclude: /node_modules/,
 }
